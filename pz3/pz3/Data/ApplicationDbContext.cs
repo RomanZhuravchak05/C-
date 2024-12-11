b964df1c21f6e53c7ebe2d34f11192499046a0ea
RomanZhuravchak05 <st8601633@stud.duikt.edu.ua>
2024-12-11 10:46:30 +0200

    Add files via upload

---
+++ pz3/pz3/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
