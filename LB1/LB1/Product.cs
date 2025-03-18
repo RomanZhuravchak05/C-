4a145e323c5526f5cbf2d3f5174d99bd189a63b4
RomanZhuravchak05 <st8601633@stud.duikt.edu.ua>
2025-03-18 21:28:11 +0200

    Add files via upload

---
+++ LB1/LB1/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+class Product
+{
+    private string _name;
+    private decimal _price;
+    private int _quantity;
+
+    public Product(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        _quantity = quantity;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                _name = value;
+            else
+                throw new ArgumentException("Назва товару не може бути порожньою.");
+        }
+    }
+
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (value >= 0)
+                _price = value;
+            else
+                throw new ArgumentException("Ціна не може бути від'ємною.");
+        }
+    }
+
+    public int Quantity => _quantity;
+    public decimal TotalValue => _price * _quantity;
+
+    public void Restock(int amount)
+    {
+        if (amount > 0)
+            _quantity += amount;
+        else
+            throw new ArgumentException("Кількість для поповнення має бути додатною.");
+    }
+
+    public void Sell(int amount)
+    {
+        if (amount > 0 && amount <= _quantity)
+            _quantity -= amount;
+        else
+            Console.WriteLine("Недостатньо товару на складі!");
+    }
+
+    public string GetInfo()
+    {
+        var culture = new CultureInfo("uk-UA");
+        return $"Товар: {_name}, Ціна: {_price.ToString("C", culture)}, Кількість: {_quantity}, Загальна вартість: {TotalValue.ToString("C", culture)}";
+    }
+}
---
+++ LB1/LB1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("uk-UA");
+        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("uk-UA");
+
+        var apple = new Product("Яблуко", 5, 100);
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Sell(20);
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Restock(50);
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Price = 7;
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Name = "Зелене яблуко";
+        Console.WriteLine(apple.GetInfo());
+
+        try
+        {
+            apple.Price = -10;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            apple.Name = "";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        apple.Sell(200);
+    }
+}
